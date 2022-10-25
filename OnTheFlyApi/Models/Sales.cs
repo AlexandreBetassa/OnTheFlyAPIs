722a1f5c58a5b3a1a93507f88edc4ba48eae69ab
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-25 11:30:25 -0300

    <Build>DatabaseSettings

--- OnTheFlyApi/Models/Sales.cs
+++ OnTheFlyApi/Models/Sales.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Models
 {
     internal class Sales
     {
+        public  Flights Flight { get; set; }
+        public List<Passengers> passenger { get; set; }
+        public bool Reserved { get; set; }
+        public bool Sold { get; set; }
     }
 }
