5136c09987b974e9dfc08a1d0266ea122871d49e
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-25 12:58:47 -0300

    <Build>Validators classe Addres

--- OnTheFlyApi/Models/Address.cs
+++ OnTheFlyApi/Models/Address.cs
@@ -10,16 +10,16 @@ namespace Models
     internal class Adress
     {
         [Required]
-        [StringLength(9)]
+        [StringLength(9, ErrorMessage = "Tamanho máximo do campo ZipCode 9 caracteres")]
         public string ZipCode { get; set; }
-        [StringLength(100)]
+        [StringLength(100, ErrorMessage = "Tamanho máximo do campo Logradouro 100 caracteres")]
         public string Logradouro { get; set; }
         public int Number { get; set; }
-        [StringLength(10)]
+        [StringLength(10, ErrorMessage = "Tamanho máximo do campo Complement 10 caracteres")]
         public string Complement { get; set; }
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage = "Tamanho máximo do campo City 30 caracteres")]
         public string City { get; set; }
-        [StringLength(2)]
+        [StringLength(2, ErrorMessage = "Tamanho máximo do campo State 2 caracteres")]
         public string State { get; set; }
     }
 }
