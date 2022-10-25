111517fd794516b8c345630e6a87c818520dd6a4
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-25 12:54:53 -0300

    <Build>Validators nas Properties de Sales

--- OnTheFlyApi/Models/Sales.cs
+++ OnTheFlyApi/Models/Sales.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace Models
 {
     internal class Sales
     {
-        public  Flights Flight { get; set; }
-        public List<Passengers> passenger { get; set; }
-        public bool Reserved { get; set; }
-        public bool Sold { get; set; }
+        [Required(ErrorMessage = "Não há voos vinculados a esta venda")]
+        public Flights Flight { get; set; }
+        [Required(ErrorMessage = "Não a pasageiros vinculados a esta venda")]
+        public List<Passengers> Passenger { get; set; } = new List<Passengers>();
+        public bool Reserved { get; set; } = false;
+        public bool Sold { get; set; } = false;
     }
 }
