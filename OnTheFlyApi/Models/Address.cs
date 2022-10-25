b44851d420020963c1a45c7864b2c4dd077f8142
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-25 14:08:16 -0300

    <Build>Consumo API viaCep

--- OnTheFlyApi/Models/Address.cs
+++ OnTheFlyApi/Models/Address.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Address
+    public class Address
     {
         [Required]
         [StringLength(9, ErrorMessage = "Tamanho máximo do campo ZipCode 9 caracteres")]
--- OnTheFlyApi/ViaCepApi/Services/ViaCepService.cs
+++ OnTheFlyApi/ViaCepApi/Services/ViaCepService.cs
@@ -1,6 +1,18 @@
-﻿namespace ViaCepApi.Services
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ViaCepApi.Services
 {
     public class ViaCepService
     {
+        public async Task<string> GetAdress(string cep)
+        {
+            using (HttpClient _adressClient = new HttpClient())
+            {
+                HttpResponseMessage response = await _adressClient.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                var adress = await response.Content.ReadAsStringAsync();
+                return adress;
+            }
+        }
     }
 }
