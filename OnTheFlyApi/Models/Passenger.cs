4f840547a98aa920f69881ecf1e58a14431e4846
Daniel Victor Santos Silva <100080815+Danivictorsilva@users.noreply.github.com>
2022-10-25 14:11:22 -0300

    add: classes Passenger, Address, PassengerService e DatabaseSettings

--- OnTheFlyApi/Models/Passenger.cs
+++ OnTheFlyApi/Models/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace Models
 {
     public class Passenger
     {
+        [Required]
+        [MaxLength(14)]
+        public string CPF { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        public char Gender { get; set; }
+        [MaxLength(14)]
+        public string Phone { get; set; }
+        [Required]
+        public DateTime DtBirth { get; set; }
+        [Required]
+        public DateTime DtRegister { get; set; }
+        [Required]
+        public bool Status { get; set; }
+        [Required]
+        public Address Address { get; set; }
+
     }
 }
---
+++ OnTheFlyApi/PassengerAPI/Utils/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PassengerAPI.Utils
+{
+    public interface IDatabaseSettings
+    {
+        string PassengerCollectionName { get; set; }
+        string DeletedPassengerCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
