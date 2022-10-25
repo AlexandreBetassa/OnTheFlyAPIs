e980b8c3a38d4df881996c6613019fccec6195a3
artur <artur@DESKTOP-V0JU5PP>
2022-10-25 14:17:21 -0300

    adicao de campos required

--- OnTheFlyApi/Models/Airport.cs
+++ OnTheFlyApi/Models/Airport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,17 @@ namespace Models
 {
     public class Airport
     {
+        []
+        [Required]
+        [StringLength(3, ErrorMessage = "Tamanho limite do campo IATA é de 3 caracteres")]
         public string IATA { get; set; }
+
+        [Required]
+        [StringLength(2, ErrorMessage = "Tamanho limite do campo State é de 2 caracteres")]
         public string State { get; set; }
+
+        [Required]
+        [StringLength(2, ErrorMessage = "Tamanho limite do campo Country é de 2 caracteres")]
         public string Country { get; set; }
     }
 }
--- OnTheFlyApi/Models/Flight.cs
+++ OnTheFlyApi/Models/Flight.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,19 @@ namespace Models
         //[BsonId]
         //[BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
 
+        [Required]
         public Airport Destiny { get; set; }
+
+        [Required]
         public AirCraft Plane { get; set; }
+
+        [Required]
         public int Sales { get; set; }
+
+        [Required]
         public DateTime Departure { get; set; }
+
+        [Required]
         public bool Status { get; set; }
     }
 }
