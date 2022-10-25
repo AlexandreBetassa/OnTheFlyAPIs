8b8b9d7d33f39b095bccfe684014c1ba2917e267
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-25 11:28:03 -0300

    Add- Empty AirCraftController

---
+++ OnTheFlyApi/AirCraftAPI/Controllers/AirCraftController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirCraftAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirCraftController : ControllerBase
+    {
+    }
+}
