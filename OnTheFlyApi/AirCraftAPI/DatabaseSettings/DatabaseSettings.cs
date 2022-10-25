d5856694244c2380fc58e0a3181c68c23b0026d6
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-25 11:58:14 -0300

    Add- DattaBaseSettings Properties

--- OnTheFlyApi/AirCraftAPI/DatabaseSettings/DatabaseSettings.cs
+++ OnTheFlyApi/AirCraftAPI/DatabaseSettings/DatabaseSettings.cs
@@ -1,6 +1,10 @@
 ﻿namespace AirCraftAPI.DatabaseSettings
 {
-    public class DatabaseSettings
+    public class DatabaseSettings : IDatabaseSettings
     {
+        public string AirCraftCollectionName { get; set; }
+        public string DeletedAirCraftCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DataBaseName { get; set; }
     }
 }
--- OnTheFlyApi/AirCraftAPI/DatabaseSettings/IDatabaseSettings.cs
+++ OnTheFlyApi/AirCraftAPI/DatabaseSettings/IDatabaseSettings.cs
@@ -2,6 +2,9 @@
 {
     public interface IDatabaseSettings
     {
-
+        string AirCraftCollectionName { get; set; }
+        string DeletedAirCraftCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DataBaseName { get; set; }
     }
 }
