472d85fe8713fcc08eb4b1dae2790caee901178b
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-26 14:23:16 -0300

    Add- DeletedAircraft Services initial configurations

---
+++ OnTheFlyApi/AirCraftAPI/Services/DeletedAirCraftService.cs
@@ -0,0 +1,18 @@
+﻿using AirCraftAPI.DatabaseSettings;
+using Models;
+using MongoDB.Driver;
+
+namespace AirCraftAPI.Services
+{
+    public class DeletedAirCraftService
+    {
+        private readonly IMongoCollection<AirCraft> _deletedAircraft;
+        
+        public DeletedAirCraftService(IDatabaseSettings settings)
+        {
+            var deletedAircraft = new MongoClient(settings.ConnectionString);
+            var database = deletedAircraft.GetDatabase(settings.DataBaseName);
+            _deletedAircraft = database.GetCollection<AirCraft>(settings.DeletedAirCraftCollectionName);
+        }
+    }
+}
