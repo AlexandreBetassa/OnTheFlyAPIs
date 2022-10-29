7cbc2326cff9aa09da689b4524d8505f6650ca0b
artur <artur@DESKTOP-V0JU5PP>
2022-10-28 22:38:28 -0300

    adicionadas as verificacoes de voo

--- OnTheFlyApi/FlightsAPI/Controllers/FlightController.cs
+++ OnTheFlyApi/FlightsAPI/Controllers/FlightController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using APIsConsummers;
 using System.IO;
+using System.Linq;
 
 namespace FlightsAPI.Controllers
 {
@@ -28,8 +29,7 @@ namespace FlightsAPI.Controllers
         {
             var flight = _flightService.GetByDate(date);
 
-            if (flight == null)
-                return NotFound();
+            if (flight == null) return NotFound();
 
             return Ok(flight);
         }
@@ -39,27 +39,30 @@ namespace FlightsAPI.Controllers
         {
             var flight = _flightService.GetOne(fullDate, rabPlane, destiny);
 
-            if (flight == null)
-                return NotFound();
+            if (flight == null) return NotFound();
 
             return Ok(flight);
         }
 
-        //verificar a existencia de companhia, aeronave e aeroporto
-        //não pode cadastrar voos para uma companhia que esteja bloqueada (fazer tal verificação)
         [HttpPost("{rab}/{destiny}/{dateFlight}")]
         public ActionResult<Flight> Create(string rab, DateTime dateFlight, string destiny)
         {
-            AirCraft airCraft = AirCraftAPIConsummer.GetAirCraft(rab).Result;
+            AirCraft airCraft = AirCraftAPIConsummer.GetAirCraft(rab.ToUpper()).Result;
             if (airCraft == null) return NotFound();
             if (airCraft.Company.Status == true) return BadRequest("Restricted Airline, flights can only be registered for unrestricted airlines.");
 
-            Airport airport = new Airport{Country="BR", State = "SP", IATA = destiny}; /*Consumo api pestana*/
+            Airport airport = new Airport { Country = "BR", State = "SP", IATA = destiny.ToUpper() }; /*Consumo api pestana*/
             if (airport == null) return NotFound();
 
             if (dateFlight < DateTime.Now) return BadRequest("Invalid Date, the date must be a future date the current date.");
 
             Flight flight = new() { Plane = airCraft, Departure = dateFlight, Destiny = airport, Sales = 0, Status = true };
+
+            var lstFlight = _flightService.Get();
+            var flighOfListWithEqualInformation = lstFlight.FirstOrDefault(f => f.Plane.RAB == flight.Plane.RAB && f.Departure == dateFlight);
+
+            if (flighOfListWithEqualInformation != null)
+                if (flighOfListWithEqualInformation.Plane.RAB == flight.Plane.RAB && flighOfListWithEqualInformation.Departure == flight.Departure) return BadRequest("This flight information already exists, cannot register more than one flight with the same plane on the same date.");
 
             _flightService.Create(flight);
 
@@ -71,8 +74,7 @@ namespace FlightsAPI.Controllers
         {
             var flightUpdate = _flightService.GetOne(flight.Departure, flight.Plane.RAB, flight.Destiny.IATA);
 
-            if (flightUpdate == null)
-                return NotFound();
+            if (flightUpdate == null) return NotFound();
 
             _flightService.UpdateSales(flight.Departure, flight.Plane.RAB, flight.Destiny.IATA, flightUpdate);
 
@@ -83,8 +85,7 @@ namespace FlightsAPI.Controllers
         public ActionResult<Flight> UpdateStatus(DateTime fullDate, string rabPlane, string destiny, bool newStatus)
         {
             var flightUpdate = _flightService.GetOne(fullDate, rabPlane, destiny);
-            if (flightUpdate == null)
-                return NotFound();
+            if (flightUpdate == null) return NotFound();
 
             flightUpdate.Status = newStatus;
 
